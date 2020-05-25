efc7426a8e7f9127a307503c75d95551b9dcd9ea
Jerry <georgemi@ymail.com>
2020-05-25 12:36:21 -0400

    publisher Create API working d4

---
+++ src/main/java/com/jm/libraryapis - Copy/libraryapis/exception/LibraryResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.jm.libraryapis.libraryapis.exception;
+
+public class LibraryResourceNotFoundException extends Exception {
+    public LibraryResourceNotFoundException(String message) {
+        super(message);
+    }
+}
