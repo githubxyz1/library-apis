087e0ded4e68e46c83a76801f2eed8ce7d6397f3
Jerry <georgemi@ymail.com>
2020-05-26 18:03:54 -0400

    Publisher Get API working b2

--- src/main/java/com/jm/libraryapis - Copy/libraryapis/exception/LibraryResourceAlreadyExistException.java
+++
@@ -1,7 +0,0 @@
-package com.jm.libraryapis.libraryapis.exception;
-
-public class LibraryResourceAlreadyExistException extends Exception {
-    public LibraryResourceAlreadyExistException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/jm/libraryapis - Copy/libraryapis/exception/LibraryResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.jm.libraryapis.libraryapis.exception;
-
-public class LibraryResourceNotFoundException extends Exception {
-    public LibraryResourceNotFoundException(String message) {
-        super(message);
-    }
-}
